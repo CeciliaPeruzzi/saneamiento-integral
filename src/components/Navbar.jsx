06da8c9ad0a43bd71822882872435f5eced0dc90
cecilia-peruzzi <ceciliaperuzzi10@gmail.com>
2025-03-31 21:15:06 -0300

    Format mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 "use client";
 
-import React, { Suspense } from 'react';
+import React, { useState, Suspense } from 'react';
 import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
 import i18next from '../i18n'; 
 import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { FaBars } from 'react-icons/fa';  // Importamos el ícono de menú hamburguesa
 import '../css/navbar.css';
 
 const Navbar = () => {
@@ -17,6 +18,7 @@ const Navbar = () => {
 
 const NavbarContent = () => {
   const { t } = useTranslation('common');
+  const [isOpen, setIsOpen] = useState(false); // Controlar la visibilidad del menú hamburguesa
   
   const router = useRouter();
   const pathname = usePathname();
@@ -32,18 +34,31 @@ const NavbarContent = () => {
     }
   };
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <div className="logo-container">
           <Image src="/images/logos.png" alt="Saneamiento Integral" width={70} height={60} />
         </div>
-        <ul className="nav-links">
-          <li><a href="#nosotros">{t('navbar.nosotros')}</a></li>
-          <li><a href="#servicios">{t('navbar.servicios')}</a></li>
-          <li><a href="#empresas">{t('navbar.empresas')}</a></li>
-          <li><a href="#contacto">{t('navbar.contacto')}</a></li>
+
+        {/* Menú hamburguesa visible solo en mobile */}
+        <div className="mobile-menu-icon" onClick={toggleMenu}>
+          <FaBars size={24} />
+        </div>
+
+        {/* Links de navegación */}
+        <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+          <li><a href="#nosotros" onClick={toggleMenu}>{t('navbar.nosotros')}</a></li>
+          <li><a href="#servicios" onClick={toggleMenu}>{t('navbar.servicios')}</a></li>
+          <li><a href="#empresas" onClick={toggleMenu}>{t('navbar.empresas')}</a></li>
+          <li><a href="#contacto" onClick={toggleMenu}>{t('navbar.contacto')}</a></li>
         </ul>
+
+        {/* Selector de idioma */}
         <div className="language-switcher">
           <button onClick={() => changeLanguage('es')} className={currentLocale === 'es' ? 'active' : ''}>ES</button>
           <button onClick={() => changeLanguage('en')} className={currentLocale === 'en' ? 'active' : ''}>EN</button>
